7dadcb362846020984a4fee8bed0bff4f778d2ab
Amin Mirlohi <aminmirlohi14@gmail.com>
2026-04-17 16:33:56 -0400

    did custom hook

--- services/useFetch.ts
+++ services/useFetch.ts
@@ -16,7 +16,6 @@ const useFetch = <T>(fetchFunction: () => Promise<T>, autoFetch = true) => {
 
             setData(result);
         } catch (err) {
-            // @ts-ignore
             setError(err instanceof Error ? err : new Error('An error occurred'));
         } finally {
             setLoading(false);
